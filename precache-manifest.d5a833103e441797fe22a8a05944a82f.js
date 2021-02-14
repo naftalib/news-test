75ce6d04577ea0c0bd4180b9f022e0c07083147d
naftalib <45719650+naftalib@users.noreply.github.com>
2021-02-15 01:38:47 +0200

    Updates

---
+++ precache-manifest.d5a833103e441797fe22a8a05944a82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457bd91b6cae629b553c94c3a631bd4b",
+    "url": "/news-test/index.html"
+  },
+  {
+    "revision": "1ca4e1ee669a2d7b72e7",
+    "url": "/news-test/static/css/main.7e45420d.chunk.css"
+  },
+  {
+    "revision": "4762b54b3eb9ceea744e",
+    "url": "/news-test/static/js/2.dca65410.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-test/static/js/2.dca65410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca4e1ee669a2d7b72e7",
+    "url": "/news-test/static/js/main.f2aba03d.chunk.js"
+  },
+  {
+    "revision": "9e6407f46529eea1cd3e",
+    "url": "/news-test/static/js/runtime-main.4a643712.js"
+  }
+]);
